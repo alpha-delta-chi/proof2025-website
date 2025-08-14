e6ba87ef267d176458616a2ea24fc571e6ec6c54
alpha-delta-chi <172929924+alpha-delta-chi@users.noreply.github.com>
2025-08-14 03:19:14 +0200

    a: modified map marker, pt. 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,18 +7,29 @@ window.conference.awaitReady().then(() => {
     const map = window.conference.map.getMap();
 
     if (typeof map !== 'undefined') {
+        // let main_entrance = L.marker([51.05350121390813, 3.726539493169336]).addTo(map);
         let main_entrance = L.marker([51.05350121390813, 3.726539493169336], {
             icon: L.divIcon({
-                className: '',
-                html: '<center><span class="bg-light p-1 rounded-pill"><span class="fas fa-door-open"></span><br>Sint-Baafshuis</span></center>',
-                iconSize: [0, 0]
+                className: 'map-marker-main-entrance',
+                html: '<span style="font-size: 24px; color: red"><span class="fas fa-map-marker-alt"></span></span>',
+                // html: '<center><span class="bg-light p-1 rounded-pill"><span class="fas fa-door-open"></span><br>Sint-Baafshuis</span></center>',
+                iconSize: [24, 24],
+                iconAnchor: [12, 24]
             })
         }).addTo(map);
-        let cathedral = L.marker([51.053002820821646, 3.7270904242024625], {
+
+        function on_main_entrance_click(e) {
+            ;
+        }
+        main_entrance.on('click', on_main_entrance_click);
+
+        let cathedral = L.marker([51.05310994556973, 3.7264174309183042], {
             icon: L.divIcon({
                 className: '',
-                html: '<center><span class="bg-light p-1 rounded-pill"><span class="fas fa-church"></span><br>Saint Bavo\'s Cathedral </span></center>',
-                iconSize: [0, 0]
+                html: '<span style="font-size: 16px; color: brown"><span class="fas fa-church"></span></span>',
+                // html: '<center><span class="bg-light p-1 rounded-pill"><span class="fas fa-church"></span><br>Saint Bavo\'s Cathedral </span></center>',
+                iconSize: [16, 16],
+                iconAnchor: [0, 8]
             })
         }).addTo(map);
     }
